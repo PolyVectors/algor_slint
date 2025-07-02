b87d65ec225c4c327a459fe3cab8ab2e7ff44ce9
PolyVectors <PolyVector@protonmail.com>
2025-07-02 02:58:09 +0100

    movin up in the world

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,10 @@ slint::include_modules!();
 
 fn main() -> Result<(), slint::PlatformError> {
     let main_window = MainWindow::new()?;
+
+    main_window.set_env_name(env!("CARGO_PKG_NAME").into());
+    main_window.set_env_version(env!("CARGO_PKG_VERSION").into());
+    main_window.set_env_description(env!("CARGO_PKG_DESCRIPTION").into());
+
     main_window.run()
 }
